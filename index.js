9dc400c159bb7a921c6af9df3c481e25ae205a28
bergi <bergi@axolotlfarm.org>
2018-10-26 01:56:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const base = require('barnard59-base')
+const core = require('barnard59-core')
+const formats = require('barnard59-formats')
+
+module.exports = Object.assign({}, core, base, formats)
