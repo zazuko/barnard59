a13376aa20959d5b0110d073a39fb988c8300b91
bergi <bergi@axolotlfarm.org>
2018-11-05 00:20:05 +0100

    added protocols package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const base = require('barnard59-base')
 const core = require('barnard59-core')
 const formats = require('barnard59-formats')
+const protocols = require('barnard59-protocols')
 
-module.exports = Object.assign({}, core, base, formats)
+module.exports = Object.assign({}, core, base, formats, protocols)
