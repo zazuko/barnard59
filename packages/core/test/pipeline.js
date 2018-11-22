bd065b5ad52647fe166fffd346d2d5b87b8e22f3
tpluscode <tpluscode@users.noreply.github.com>
2018-11-22 11:50:41 +0100

    test loading multiple pipelines

--- packages/core/test/pipeline.js
+++ packages/core/test/pipeline.js
@@ -16,5 +16,29 @@ describe('pipeline', () => {
       // then
       assert.deepStrictEqual(pipeline.node.term, ns.pipeline('empty'))
     })
+
+    it('loads selected pipeline when there are multiple', async () => {
+      // given
+      const definition = await load('multiple.ttl')
+
+      // when
+      const pipeline = Pipeline(definition, {
+        iri: ns.pipeline('pipelineB')
+      })
+
+      // then
+      assert.ok(pipeline)
+    })
+
+    it('throws when multiple pipelines are found without specifying the iri', async () => {
+      // given
+      const definition = await load('multiple.ttl')
+
+      // then
+      assert.throws(() => {
+        // when
+        Pipeline(definition)
+      })
+    })
   })
 })
