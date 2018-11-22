2faa068268e8cebd75a06b45fb2b892051bc79d6
tpluscode <tpluscode@users.noreply.github.com>
2018-11-22 12:12:06 +0100

    replace mocha with jest

--- packages/core/test/pipeline.test.js
+++ packages/core/test/pipeline.test.js
@@ -1,4 +1,4 @@
-/* global describe, it */
+/* global describe, test */
 const assert = require('assert')
 const Pipeline = require('../lib/pipeline')
 const load = require('./support/load-pipeline')
@@ -6,7 +6,7 @@ const ns = require('./support/namespaces')
 
 describe('pipeline', () => {
   describe('constructor', () => {
-    it('loads the single pipeline from definition', async () => {
+    test('loads the single pipeline from definition', async () => {
       // given
       const definition = await load('empty.ttl')
 
@@ -17,7 +17,7 @@ describe('pipeline', () => {
       assert.deepStrictEqual(pipeline.node.term, ns.pipeline('empty'))
     })
 
-    it('loads selected pipeline when there are multiple', async () => {
+    test('loads selected pipeline when there are multiple', async () => {
       // given
       const definition = await load('multiple.ttl')
 
@@ -30,7 +30,7 @@ describe('pipeline', () => {
       assert.ok(pipeline)
     })
 
-    it('throws when multiple pipelines are found without specifying the iri', async () => {
+    test('throws when multiple pipelines are found without specifying the iri', async () => {
       // given
       const definition = await load('multiple.ttl')
 
