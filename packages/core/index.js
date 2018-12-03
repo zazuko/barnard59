887f6ac665300d55d4132ae27af4de54475d4a96
tpluscode <tpluscode@users.noreply.github.com>
2018-12-03 12:14:24 +0100

    export factory in entrypoint to not change behavior

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,5 +1,5 @@
 const forEach = require('./lib/forEach')
-const pipeline = require('./lib/pipeline')
+const pipeline = require('./lib/pipeline').create
 const run = require('./lib/run')
 
 module.exports = {
