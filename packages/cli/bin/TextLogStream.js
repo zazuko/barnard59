575a52d900aa9c2532a907032b3de64fc0e0fc85
tpluscode <tpluscode@users.noreply.github.com>
2019-01-09 11:59:56 +0100

    logging to console (behind flag)

---
+++ packages/cli/bin/TextLogStream.js
@@ -0,0 +1,16 @@
+const Transform = require('readable-stream').Transform
+
+class TextLogStream extends Transform {
+  constructor () {
+    super({ objectMode: true })
+  }
+
+  _transform (chunk, e, next) {
+    if (chunk.name) {
+      this.push(`${chunk.level} ${chunk.stack[0]} ${chunk.message}\r\n`)
+    }
+    next()
+  }
+}
+
+module.exports = TextLogStream
