2d70c526c95b14a03bf31abc82d93ae7843c4251
tpluscode <tpluscode@users.noreply.github.com>
2019-03-07 13:11:23 +0100

    mock API calls in world date time tests

--- packages/core/test/pipeline.e2e.test.js
+++ packages/core/test/pipeline.e2e.test.js
@@ -1,11 +1,23 @@
-/* global describe, test */
 const expect = require('expect')
 const Pipeline = require('../lib/pipelineFactory')
 const load = require('./support/load-pipeline')
 const run = require('./support/run')
 const asyncLoaders = require('./support/asyncLoaders')
+const fetch = require('jest-fetch-mock')
+
+jest.setMock('node-fetch', fetch)
 
 describe('Pipeline', () => {
+  beforeEach(() => {
+    fetch.resetMocks()
+
+    const dateTime = {
+      'datetime': '2019-03-07T12:58:54.094127+01:00'
+    }
+
+    fetch.mockResponse(JSON.stringify(dateTime))
+  })
+
   test('can load code using node: scheme', async () => {
     // given
     const definition = await load('e2e/world-clock-node.ttl')
