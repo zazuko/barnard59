4d5a2d059f8b95befb9f886e3074c1f81da2ff91
bergi <bergi@axolotlfarm.org>
2019-09-18 00:04:12 +0200

    removed formats, protocols and shell dependency

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const base = require('barnard59-base')
 const core = require('barnard59-core')
-const formats = require('barnard59-formats')
-const protocols = require('barnard59-protocols')
 
-module.exports = Object.assign({}, core, base, formats, protocols)
+module.exports = Object.assign({}, core, base)
