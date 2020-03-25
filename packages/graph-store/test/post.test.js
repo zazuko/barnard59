da6abdb3bf7e991619dfacd6c19a5ad010a6e70f
bergi <bergi@axolotlfarm.org>
2020-03-26 00:42:02 +0100

    use sparql-http-client + added get operation

--- packages/graph-store/test/post.test.js
+++ packages/graph-store/test/post.test.js
@@ -1,244 +1,237 @@
-/* global describe, expect, test */
-
-const { post } = require('..')
-const { isReadable, isWritable } = require('isstream')
-const namespace = require('@rdfjs/namespace')
-const rdf = require('@rdfjs/data-model')
-const { finished } = require('readable-stream')
-const streamToString = require('./support/streamToString')
-const { quadToNTriples } = require('@rdfjs/to-ntriples')
+const { strictEqual } = require('assert')
 const { promisify } = require('util')
-const ExpressAsPromise = require('express-as-promise')
+const getStream = require('get-stream')
+const { isReadable, isWritable } = require('isstream')
+const { describe, it } = require('mocha')
+const rdf = require('@rdfjs/data-model')
+const namespace = require('@rdfjs/namespace')
+const { quadToNTriples } = require('@rdfjs/to-ntriples')
+const { finished } = require('readable-stream')
+const withServer = require('./support/withServer')
+const { post } = require('..')
 
 const ns = namespace('http://example.org/')
 
 describe('post', () => {
-  test('returns a writable stream', async () => {
-    const server = new ExpressAsPromise()
-    const baseUrl = await server.listen()
+  it('should return a writable stream', async () => {
+    await withServer(async server => {
+      const baseUrl = await server.listen()
 
-    const stream = await post({ endpoint: baseUrl })
+      const stream = post({ endpoint: baseUrl })
 
-    expect(isReadable(stream)).toBe(false)
-    expect(isWritable(stream)).toBe(true)
+      strictEqual(isReadable(stream), false)
+      strictEqual(isWritable(stream), true)
 
-    stream.end()
-
-    await server.stop()
+      stream.end()
+    })
   })
 
-  test('sends a POST request', async () => {
-    let called = false
-    const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
+  it('should send a GET request', async () => {
+    await withServer(async server => {
+      let called = false
+      const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
 
-    const server = new ExpressAsPromise()
+      server.app.post('/', async (req, res) => {
+        called = true
 
-    server.app.post('/', async (req, res) => {
-      called = true
+        res.status(204).end()
+      })
 
-      res.status(204).end()
-    })
+      const baseUrl = await server.listen()
+      const stream = post({ endpoint: baseUrl })
 
-    const baseUrl = await server.listen()
-    const stream = await post({ endpoint: baseUrl })
-
-    stream.write(quad)
-    stream.end()
-
-    await promisify(finished)(stream)
-
-    expect(called).toBe(true)
-
-    await server.stop()
-  })
-
-  test('sends content with media type application/n-triples to the server', async () => {
-    let mediaType = null
-    const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
-
-    const server = new ExpressAsPromise()
-
-    server.app.post('/', async (req, res) => {
-      mediaType = req.get('content-type')
-
-      res.status(204).end()
-    })
-
-    const baseUrl = await server.listen()
-    const stream = await post({ endpoint: baseUrl })
-
-    stream.write(quad)
-    stream.end()
-
-    await promisify(finished)(stream)
-
-    expect(mediaType).toBe('application/n-triples')
-
-    await server.stop()
-  })
-
-  test('sends the quad stream as N-Triples to the server', async () => {
-    const content = {}
-    const quads = [
-      rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1),
-      rdf.quad(ns.subject2, ns.predicate2, ns.object2, ns.graph1),
-      rdf.quad(ns.subject3, ns.predicate3, ns.object3, ns.graph1),
-      rdf.quad(ns.subject4, ns.predicate4, ns.object4, ns.graph1)
-    ]
-    const expected = quads.map(quad => {
-      return quadToNTriples(rdf.quad(quad.subject, quad.predicate, quad.object)) + '\n'
-    }).join('')
-
-    const server = new ExpressAsPromise()
-
-    server.app.post('/', async (req, res) => {
-      content[req.query.graph] = await streamToString(req)
-
-      res.status(204).end()
-    })
-
-    const baseUrl = await server.listen()
-    const stream = await post({ endpoint: baseUrl })
-
-    quads.forEach(quad => {
       stream.write(quad)
+      stream.end()
+
+      await promisify(finished)(stream)
+
+      strictEqual(called, true)
     })
-
-    stream.end()
-
-    await promisify(finished)(stream)
-
-    expect(content[quads[0].graph.value]).toBe(expected)
-
-    await server.stop()
   })
 
-  test('supports default graph', async () => {
-    let graph = true
-    let content = {}
-    const quads = [
-      rdf.quad(ns.subject1, ns.predicate1, ns.object1),
-      rdf.quad(ns.subject2, ns.predicate2, ns.object2),
-      rdf.quad(ns.subject3, ns.predicate3, ns.object3),
-      rdf.quad(ns.subject4, ns.predicate4, ns.object4)
-    ]
-    const expected = quads.map(quad => {
-      return quadToNTriples(rdf.quad(quad.subject, quad.predicate, quad.object)) + '\n'
-    }).join('')
+  it('should send a content-type header with the value application/n-triples', async () => {
+    await withServer(async server => {
+      let mediaType = null
+      const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
 
-    const server = new ExpressAsPromise()
+      server.app.post('/', async (req, res) => {
+        mediaType = req.get('content-type')
 
-    server.app.post('/', async (req, res) => {
-      graph = req.query.graph
-      content = await streamToString(req)
+        res.status(204).end()
+      })
 
-      res.status(204).end()
-    })
+      const baseUrl = await server.listen()
+      const stream = post({ endpoint: baseUrl })
 
-    const baseUrl = await server.listen()
-    const stream = await post({ endpoint: baseUrl })
-
-    quads.forEach(quad => {
       stream.write(quad)
+      stream.end()
+
+      await promisify(finished)(stream)
+
+      strictEqual(mediaType, 'application/n-triples')
     })
-
-    stream.end()
-
-    await promisify(finished)(stream)
-
-    expect(graph).toBe(undefined)
-    expect(content).toBe(expected)
-
-    await server.stop()
   })
 
-  test('should use multiple request to send multiple graphs', async () => {
-    const content = {}
-    const quads = [
-      rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1),
-      rdf.quad(ns.subject2, ns.predicate2, ns.object2, ns.graph1),
-      rdf.quad(ns.subject3, ns.predicate3, ns.object3),
-      rdf.quad(ns.subject4, ns.predicate4, ns.object4),
-      rdf.quad(ns.subject5, ns.predicate5, ns.object5, ns.graph2),
-      rdf.quad(ns.subject6, ns.predicate6, ns.object6, ns.graph2)
-    ]
-    const expected = quads.reduce((expected, quad) => {
-      const graphIri = quad.graph.value || 'default'
+  it('should send the quad stream as N-Triples', async () => {
+    await withServer(async server => {
+      const content = {}
+      const quads = [
+        rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1),
+        rdf.quad(ns.subject2, ns.predicate2, ns.object2, ns.graph1),
+        rdf.quad(ns.subject3, ns.predicate3, ns.object3, ns.graph1),
+        rdf.quad(ns.subject4, ns.predicate4, ns.object4, ns.graph1)
+      ]
+      const expected = quads.map(quad => {
+        return quadToNTriples(rdf.quad(quad.subject, quad.predicate, quad.object)) + '\n'
+      }).join('')
 
-      expected[graphIri] = (expected[graphIri] || '') +
-        quadToNTriples(rdf.quad(quad.subject, quad.predicate, quad.object)) + '\n'
+      server.app.post('/', async (req, res) => {
+        content[req.query.graph] = await getStream(req)
 
-      return expected
-    }, {})
+        res.status(204).end()
+      })
 
-    const server = new ExpressAsPromise()
+      const baseUrl = await server.listen()
+      const stream = post({ endpoint: baseUrl })
 
-    server.app.post('/', async (req, res) => {
-      content[typeof req.query.graph === 'string' ? req.query.graph : 'default'] = await streamToString(req)
+      quads.forEach(quad => {
+        stream.write(quad)
+      })
 
-      res.status(204).end()
+      stream.end()
+
+      await promisify(finished)(stream)
+
+      strictEqual(content[quads[0].graph.value], expected)
     })
+  })
 
-    const baseUrl = await server.listen()
-    const stream = await post({ endpoint: baseUrl })
+  it('should support default graph', async () => {
+    await withServer(async server => {
+      let graph = true
+      let content = {}
+      const quads = [
+        rdf.quad(ns.subject1, ns.predicate1, ns.object1),
+        rdf.quad(ns.subject2, ns.predicate2, ns.object2),
+        rdf.quad(ns.subject3, ns.predicate3, ns.object3),
+        rdf.quad(ns.subject4, ns.predicate4, ns.object4)
+      ]
+      const expected = quads.map(quad => {
+        return quadToNTriples(rdf.quad(quad.subject, quad.predicate, quad.object)) + '\n'
+      }).join('')
 
-    quads.forEach(quad => {
+      server.app.post('/', async (req, res) => {
+        graph = req.query.graph
+        content = await getStream(req)
+
+        res.status(204).end()
+      })
+
+      const baseUrl = await server.listen()
+      const stream = post({ endpoint: baseUrl })
+
+      quads.forEach(quad => {
+        stream.write(quad)
+      })
+
+      stream.end()
+
+      await promisify(finished)(stream)
+
+      strictEqual(typeof graph, 'undefined')
+      strictEqual(content, expected)
+    })
+  })
+
+  it('should use multiple request to send multiple graphs', async () => {
+    await withServer(async server => {
+      const content = {}
+      const quads = [
+        rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1),
+        rdf.quad(ns.subject2, ns.predicate2, ns.object2, ns.graph1),
+        rdf.quad(ns.subject3, ns.predicate3, ns.object3),
+        rdf.quad(ns.subject4, ns.predicate4, ns.object4),
+        rdf.quad(ns.subject5, ns.predicate5, ns.object5, ns.graph2),
+        rdf.quad(ns.subject6, ns.predicate6, ns.object6, ns.graph2)
+      ]
+      const expected = quads.reduce((expected, quad) => {
+        const graphIri = quad.graph.value || 'default'
+
+        expected[graphIri] = (expected[graphIri] || '') +
+          quadToNTriples(rdf.quad(quad.subject, quad.predicate, quad.object)) + '\n'
+
+        return expected
+      }, {})
+
+      server.app.post('/', async (req, res) => {
+        content[typeof req.query.graph === 'string' ? req.query.graph : 'default'] = await getStream(req)
+
+        res.status(204).end()
+      })
+
+      const baseUrl = await server.listen()
+      const stream = post({ endpoint: baseUrl })
+
+      quads.forEach(quad => {
+        stream.write(quad)
+      })
+
+      stream.end()
+
+      await promisify(finished)(stream)
+
+      Object.entries(expected).forEach(([graphIri, graphContent]) => {
+        strictEqual(graphContent, content[graphIri])
+      })
+    })
+  })
+
+  it('should send a basic authentication header if user and password are given', async () => {
+    await withServer(async server => {
+      let credentials = null
+      const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
+
+      server.app.post('/', async (req, res) => {
+        credentials = req.get('authorization')
+
+        res.status(204).end()
+      })
+
+      const baseUrl = await server.listen()
+      const stream = post({ endpoint: baseUrl, user: 'testuser', password: 'testpassword' })
+
       stream.write(quad)
+      stream.end()
+
+      await promisify(finished)(stream)
+
+      strictEqual(credentials, 'Basic dGVzdHVzZXI6dGVzdHBhc3N3b3Jk')
     })
-
-    stream.end()
-
-    await promisify(finished)(stream)
-
-    Object.entries(content).forEach(([graphIri, graphContent]) => {
-      expect(graphContent).toStrictEqual(expected[graphIri])
-    })
-
-    await server.stop()
   })
 
-  test('sends basic authentication headers', async () => {
-    let credentials = null
-    const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
+  it('should handle server errors', async () => {
+    await withServer(async server => {
+      const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
 
-    const server = new ExpressAsPromise()
+      server.app.post('/', async (req, res) => {
+        res.status(500).end()
+      })
 
-    server.app.post('/', async (req, res) => {
-      credentials = req.get('authorization')
+      const baseUrl = await server.listen()
+      const stream = post({ endpoint: baseUrl })
 
-      res.status(204).end()
+      stream.write(quad)
+      stream.end()
+
+      let error = null
+
+      try {
+        await promisify(finished)(stream)
+      } catch (err) {
+        error = err
+      }
+
+      strictEqual(error && error.message.includes('Internal Server Error'), true)
     })
-
-    const baseUrl = await server.listen()
-    const stream = await post({ endpoint: baseUrl, user: 'testuser', password: 'testpassword' })
-
-    stream.write(quad)
-    stream.end()
-
-    await promisify(finished)(stream)
-
-    expect(credentials).toBe('Basic dGVzdHVzZXI6dGVzdHBhc3N3b3Jk')
-
-    await server.stop()
-  })
-
-  test('handles server errors', async () => {
-    const quad = rdf.quad(ns.subject1, ns.predicate1, ns.object1, ns.graph1)
-
-    const server = new ExpressAsPromise()
-
-    server.app.post('/', async (req, res) => {
-      res.status(500).end()
-    })
-
-    const baseUrl = await server.listen()
-    const stream = await post({ endpoint: baseUrl })
-
-    stream.write(quad)
-    stream.end()
-
-    await expect(promisify(finished)(stream)).rejects.toThrow('500')
-
-    await server.stop()
   })
 })
