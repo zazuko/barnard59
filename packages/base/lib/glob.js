8290b5c02af6cadc702f913ba5d4ea6bb2ae23b7
bergi <bergi@axolotlfarm.org>
2020-05-12 18:02:57 +0200

    fixed glob export in index.js, removed outdated commented glob code

--- packages/base/lib/glob.js
+++ packages/base/lib/glob.js
@@ -3,34 +3,6 @@ const glob = require('glob')
 const once = require('lodash/once')
 const { Readable } = require('readable-stream')
 
-/* class Glob extends Readable {
-  constructor ({ pattern, ...options } = {}) {
-    super({
-      objectMode: true
-    })
-
-    this.pattern = pattern
-    this.options = options
-  }
-
-  async _read () {
-    const files = await promisify(glob)(this.pattern, this.options)
-
-    files.forEach(file => {
-      this.push(file)
-    })
-
-    this.push(null)
-  }
-
-  static create (options) {
-    return new Glob(options)
-  }
-}
-
-module.exports = Glob.create
-*/
-
 function factory ({ pattern, ...options }) {
   let filenames = null
 
