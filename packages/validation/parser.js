b0a072d30fd7fa2c6812ba3af35d68a3ae788f31
magdalena@surowka.ch <magdalena@surowka.ch>
2021-01-11 17:39:50 +0100

    pipeline parser

---
+++ packages/validation/parser.js
@@ -0,0 +1,85 @@
+const cf = require('clownface')
+const fromFile = require('rdf-utils-fs/fromFile')
+const rdf = require('rdf-ext')
+const namespace = require('@rdfjs/namespace')
+
+const ns = {
+  schema: namespace('http://schema.org/'),
+  rdf: namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
+  p: namespace("https://pipeline.described.at/"),
+  code: namespace("https://code.described.at/")
+}
+
+const pipelineFile = 'sample-pipelines/fetch-json-to-ntriples.ttl'
+
+async function streamToArray (stream) {
+    const chunks = []
+    return new Promise((resolve, reject) => {
+      stream.on('data', chunk => chunks.push(chunk))
+      stream.on('end', () => resolve(chunks))
+      stream.on('error', error => {
+        console.error(error);
+        reject(error);
+      })
+    })
+  }
+
+async function readGraph(file) {
+
+  const stream = await fromFile(file)
+  const quads = await streamToArray(stream)
+  const dataset = rdf.dataset(quads)
+
+  const clownfaceObj = cf({ dataset })
+
+  return clownfaceObj
+}
+
+async function getPipelines(graph) {
+
+  const pipelines = []
+  graph
+    .has(ns.rdf.type, ns.p.Pipeline)
+    .forEach(pipeline => {
+      pipelines.push(pipeline.term.value)
+    })
+
+  return pipelines
+}
+
+async function getIdentifiers(graph) {
+
+  var pipeline2identifier = {};
+
+  graph
+    .has(ns.rdf.type, ns.p.Pipeline)
+    .forEach(pipeline => {
+        steps = pipeline
+        .out(ns.p.steps)
+        .out(ns.p.stepList)
+        .list()
+
+        pipeline2identifier[pipeline.term.value] = []
+
+        for (const step of steps) {
+          const identifier = step
+            .out(ns.code.implementedBy)
+            .out(ns.code.link)
+            .term
+          pipeline2identifier[pipeline.term.value].push(identifier.value)
+        }
+    })
+
+  return pipeline2identifier
+}
+
+async function main() {
+
+  const graph = await readGraph(pipelineFile)
+  const steps = getIdentifiers(graph)
+
+  console.log(steps)
+
+}
+main()
+
