4f24deedce2a8cc73627c7cb4dc5d2dfc3e6d4cd
tpluscode <tpluscode@users.noreply.github.com>
2021-02-03 19:18:11 +0100

    feat: pass context to map callback

--- packages/base/lib/map.js
+++ packages/base/lib/map.js
@@ -1,9 +1,10 @@
 const through = require('through2')
 
 function map (func) {
+  const context = this
   return through.obj(function (chunk, encoding, callback) {
     Promise.resolve().then(() => {
-      return func(chunk, encoding)
+      return func.call(context, chunk, encoding)
     }).then((result) => {
       this.push(result)
 
