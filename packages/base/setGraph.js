7015ee2d29bfe08963dc539b92ef0896ea736a06
bergi <bergi@axolotlfarm.org>
2021-03-07 18:40:46 +0100

    feat!: removed deprecated code

--- packages/base/setGraph.js
+++
@@ -1,6 +0,0 @@
-import { namedNode } from 'rdf-ext'
-import TripleToQuadTransform from 'rdf-transform-triple-to-quad'
-
-export default function setGraph (graph) {
-  return new TripleToQuadTransform(namedNode(graph.toString()))
-}
