0dc13011d13dd8c43da5ae5740df63ef7ce17ff3
bergi <bergi@axolotlfarm.org>
2021-03-15 15:19:54 +0100

    feat: support importing env variables

--- bin/barnard59.js
+++ bin/barnard59.js
@@ -21,19 +21,36 @@ function createOutputStream (output) {
   return createWriteStream(output)
 }
 
+function setVariable (str, all) {
+  let [key, value] = str.split('=', 2)
+
+  if (typeof value === 'undefined') {
+    value = process.env[key]
+  }
+
+  return all.set(key, value)
+}
+
 program
   .command('run <filename>')
   .option('--output [filename]', 'output file', '-')
   .option('--pipeline [iri]', 'IRI of the pipeline description')
-  .option('--variable <name=value>', 'variable key value pairs', (v, all) => all.set(...v.split('=', 2)), new Map())
+  .option('--variable <name=value>', 'variable key value pairs', setVariable, new Map())
+  .option('--variable-all', 'Import all environment variables')
   .option('-v, --verbose', 'enable diagnostic console output', (v, total) => ++total, 0)
   .option('--enable-buffer-monitor', 'enable histogram of buffer usage')
-  .action(async (filename, { output, pipeline: iri, variable: variables, verbose, enableBufferMonitor } = {}) => {
+  .action(async (filename, { output, pipeline: iri, variable: variables, variableAll, verbose, enableBufferMonitor } = {}) => {
     try {
       const level = ['error', 'info', 'debug'][verbose] || 'error'
 
       const dataset = await fileToDataset(filename)
       const ptr = findPipeline(dataset, iri)
+
+      if (variableAll) {
+        for (const [key, value] of Object.entries(process.env)) {
+          variables.set(key, value)
+        }
+      }
 
       const { finished, pipeline } = await runner(ptr, {
         basePath: resolve(dirname(filename)),
