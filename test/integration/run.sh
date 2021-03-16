16b54fa0e76eede9913b39e015519eeaef14d0f5
magdalena@surowka.ch <magdalena@surowka.ch>
2021-03-16 17:46:19 +0100

    pipeline two adjustnment

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -2,4 +2,8 @@
 
 node bin/barnard59 run test/integration/one/pipeline.ttl -v \
 --pipeline http://your-domain.ld.admin.ch/pipeline/mainCreateFile \
---output results.nt
+--output test/integration/one/actual_output.nt
+
+node bin/barnard59 run test/integration/two/pipeline.ttl -v \
+--pipeline http://ld.admin.ch/pipeline/metadata/mainCreateFile \
+--output test/integration/two/actual_output.nt
