2a7a5776985706691bd2ee1124cb762ea143a221
magdalena@surowka.ch <magdalena@surowka.ch>
2021-03-22 11:52:17 +0100

    call

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-node bin/barnard59 run test/integration/one/pipeline.ttl -v \
---pipeline http://your-domain.ld.admin.ch/pipeline/mainCreateFile \
---output test/integration/one/actual_output.nt
+#node bin/barnard59 run test/integration/one/pipeline.ttl -v \
+#--pipeline http://your-domain.ld.admin.ch/pipeline/mainCreateFile \
+#--output test/integration/one/actual_output.nt
 
 node bin/barnard59 run test/integration/three/pipeline.ttl -v \
 --output test/integration/three/actual_output.nt
