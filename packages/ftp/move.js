5babbe2cc23dcb156d872c58a4bfb6d71bec31a9
bergi <bergi@axolotlfarm.org>
2021-03-31 00:11:13 +0200

    fix: use object mode in move stream wrapper

--- packages/ftp/move.js
+++ packages/ftp/move.js
@@ -3,6 +3,7 @@ const { Transform } = require('readable-stream')
 
 function move ({ source, target, ...options }) {
   return new Transform({
+    objectMode: true,
     flush: async callback => {
       await command(options, async client => {
         return client.move(source, target)
