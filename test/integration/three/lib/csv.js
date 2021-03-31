d4dd47880322fd2257b4bc2df62c75da3c4fee7d
magdalena@surowka.ch <magdalena@surowka.ch>
2021-03-31 18:30:26 +0200

    3rd pipeline

--- test/integration/three/lib/csv.js
+++ test/integration/three/lib/csv.js
@@ -1,8 +1,10 @@
-import { createReadStream } from 'fs'
+import { createReadStream, readFileSync } from 'fs'
 import path from 'path'
 
 export function openFromCsvw (csvwPath) {
-  const csvw = require(`../${csvwPath}`)
+
+  const __dirname = process.cwd()
+  const csvw = JSON.parse(readFileSync(`${__dirname}/${csvwPath}`, 'utf8'));
 
   return createReadStream(path.resolve(__dirname, csvw.url))
 }
