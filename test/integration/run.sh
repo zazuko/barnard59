30073b1f8e9eedbf42f1655b5648067f3cd3b59f
magdalena@surowka.ch <magdalena@surowka.ch>
2021-03-31 11:03:18 +0200

    2nd pipeline

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -4,9 +4,9 @@
 #--pipeline http://your-domain.ld.admin.ch/pipeline/mainCreateFile \
 #--output test/integration/one/actual_output.nt
 
-node bin/barnard59 run test/integration/three/pipeline.ttl -v \
---output test/integration/three/actual_output.nt
+#node bin/barnard59 run test/integration/three/pipeline.ttl -v \
+#--output test/integration/three/actual_output.nt
 
-#node bin/barnard59 run test/integration/two/pipeline.ttl -v \
-#--pipeline http://ld.admin.ch/pipeline/metadata/mainCreateFile \
-#--output test/integration/two/actual_output.nt
+node bin/barnard59 run test/integration/two/pipeline.ttl -v \
+--pipeline http://ld.admin.ch/pipeline/metadata/mainCreateFile \
+--output test/integration/two/actual_output.nt
