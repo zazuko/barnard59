cd5400aeb17f714dd349eadccf812f5f1af9797f
magdalena@surowka.ch <magdalena@surowka.ch>
2021-03-31 19:22:52 +0200

    integration tests

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -1,49 +1,37 @@
 #!/bin/bash
 
 for testcase in "01" "02" "03"
-# Test 01
-actual="test/integration/01/actual_output.nt"
-expected="test/integration/01/expected_output.nt"
+do
 
-node bin/barnard59 run test/integration/01/pipeline.ttl -v \
---pipeline http://your-domain.ld.admin.ch/pipeline/mainCreateFile \
---output "$actual"
+actual="test/integration/$testcase/actual_output.nt"
+expected="test/integration/$testcase/expected_output.nt"
+
+if [[ "$testcase" == "01" ]]; then
+    node bin/barnard59 run test/integration/01/pipeline.ttl \
+    --pipeline http://your-domain.ld.admin.ch/pipeline/mainCreateFile \
+    --output "$actual"
+fi
+
+if [[ "$testcase" == "02" ]]; then
+    node bin/barnard59 run test/integration/02/pipeline.ttl \
+    --pipeline http://ld.admin.ch/pipeline/metadata/mainCreateFile \
+    --output "$actual"
+fi
+
+if [[ "$testcase" == "03" ]]; then
+    node bin/barnard59 run test/integration/03/pipeline.ttl \
+    --variable="inputDir=test/integration/03/input" \
+    --variable="$actual" \
+    --variable="mappingsDir=test/integration/03/mappings/*.json" \
+    --pipeline=urn:pipeline:bar#Main
+fi
 
 if cmp -s "$actual" "$expected"; then
-    printf "Test 01 passed"
+    printf "Test $testcase passed\n"
 else
+    printf "Test $testcase failed\n"
     printf 'The file "%s" is different than "%s"\n' "$actual" "$expected"
     exit 1
 fi
 
-# Test 02
-actual="test/integration/02/actual_output.nt"
-expected="test/integration/02/expected_output.nt"
-
-node bin/barnard59 run test/integration/02/pipeline.ttl -v \
---pipeline http://ld.admin.ch/pipeline/metadata/mainCreateFile \
---output "$actual"
-
-if cmp -s "$actual" "$expected"; then
-    printf "Test 01 passed"
-else
-    printf 'The file "%s" is different than "%s"\n' "$actual" "$expected"
-    exit 1
-fi
-
-# Test 03
-actual="test/integration/03/actual_output.nt"
-expected="test/integration/03/expected_output.nt"\
-
-node bin/barnard59 run -v test/integration/03/pipeline.ttl \
---variable="inputDir=test/integration/03/input" \
---variable="$actual" \
---variable="mappingsDir=test/integration/03/mappings/*.json" \
---pipeline=urn:pipeline:bar#Main
-
-if cmp -s "$actual" "$expected"; then
-    printf "Test 01 passed"
-else
-    printf 'The file "%s" is different than "%s"\n' "$actual" "$expected"
-    exit 1
-fi
+done
--- test/integration/scope.md
+++ test/integration/scope.md
@@ -1,9 +1,9 @@
-## Frequently use functions with barnard59
+## Integration tests for barnard59 universe
 
-To produce expected files, run:
-`node bin/barnard59.js run test/integration/01/pipeline.ttl`
+To run the integration tests, run:
+`./tests/integration/run.sh`
 ### Tested:
-Test pipelines contain following modules:
+Test pipelines cover following modules:
 * `node:fs#createReadStream`
 * `node:fs#createWriteStream`
 
@@ -21,7 +21,7 @@ Test pipelines contain following modules:
 
 
 ### Not tested
-
+The following modules are not covered:
 * `node:barnard59-base#setGraph`
 * `node:barnard59-base#flatten`
 * `node:barnard59-base#json.parse`
@@ -36,32 +36,3 @@ Test pipelines contain following modules:
 * `node:barnard59-http#fetch`
 * `node:barnard59-px#parse`
 * `node:barnard59-ftp#read`
-
-
-
-One (lindas-pipeline)
-* node:barnard59-base#map
-* node:barnard59-base#concat.object
-* node:barnard59-formats#ntriples.serialize
-* node:barnard59-formats#n3.parse
-* node:fs#createReadStream
-
-Two (lindas-metadata)
-* node:barnard59-formats#n3.parse
-* node:barnard59-tdb#update
-* node:barnard59-core#forEach
-* node:barnard59-base#glob
-* node:barnard59-base#concat.object
-* node:fs#createReadStream
-
-Three (isil)
-* node:barnard59-base#concat.object
-* node:barnard59-base#filter
-* node:barnard59-base#glob
-* node:barnard59-core#forEach
-* node:barnard59-formats#ntriples.serialize
-* node:barnard59-formats#csvw.parse
-* node:barnard59-formats#jsonld.parse
-* node:fs#createWriteStream
-* custom code
-
