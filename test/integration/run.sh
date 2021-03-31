5d8d443ccd7ab8f9331ed3dddb68af835b6aafa5
magdalena@surowka.ch <magdalena@surowka.ch>
2021-03-31 15:54:46 +0200

    3rd pipeline files

--- test/integration/run.sh
+++ test/integration/run.sh
@@ -3,10 +3,17 @@
 #node bin/barnard59 run test/integration/one/pipeline.ttl -v \
 #--pipeline http://your-domain.ld.admin.ch/pipeline/mainCreateFile \
 #--output test/integration/one/actual_output.nt
+#
+#node bin/barnard59 run test/integration/two/pipeline.ttl -v \
+#--pipeline http://ld.admin.ch/pipeline/metadata/mainCreateFile \
+#--output test/integration/two/actual_output.nt
 
 #node bin/barnard59 run test/integration/three/pipeline.ttl -v \
-#--output test/integration/three/actual_output.nt
+#--pipeline=urn:pipeline:bar#Main
 
-node bin/barnard59 run test/integration/two/pipeline.ttl -v \
---pipeline http://ld.admin.ch/pipeline/metadata/mainCreateFile \
---output test/integration/two/actual_output.nt
+node bin/barnard59 run -v test/integration/three/pipeline.ttl \
+--variable="inputDir=test/integration/three/input" \
+--variable="targetFile=test/integration/three/actual_output.nt" \
+--variable="mappingsDir=test/integration/three/mappings/*.json" \
+--pipeline=urn:pipeline:bar#Main
+
