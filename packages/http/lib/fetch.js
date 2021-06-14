e6f997a8800f0662f3205199ff7676ba9e3a7199
Quentin Gliech <quentingliech@gmail.com>
2021-06-14 10:37:08 +0200

    feat: opentelemetry tracing

--- packages/http/lib/fetch.js
+++ packages/http/lib/fetch.js
@@ -1,10 +1,21 @@
+import { SpanStatusCode } from '@opentelemetry/api'
 import toReadable from 'duplex-to/readable.js'
 import nodeFetch from 'node-fetch'
+import tracer from './tracer.js'
 
 async function fetch ({ method = 'GET', url, ...options } = {}) {
-  const response = await nodeFetch(url, { method, ...options })
+  return await tracer.startActiveSpan('fetch', async span => {
+    try {
+      const response = await nodeFetch(url, { method, ...options })
 
-  return toReadable(response.body)
+      return toReadable(response.body)
+    } catch (e) {
+      span.recordException(e)
+      span.setStatus({ code: SpanStatusCode.ERROR, message: e.message })
+    } finally {
+      span.end()
+    }
+  })
 }
 
 export default fetch
