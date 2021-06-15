zazuko/barnard59-rdf

refactor: move operations to ESM

test/setGraph.test.js
@@ -1,11 +1,11 @@
-const { strictEqual } = require('assert')
-const getStream = require('get-stream')
-const intoStream = require('into-stream')
-const { isDuplex } = require('isstream')
-const { describe, it } = require('mocha')
-const rdf = require('rdf-ext')
-const ns = require('./support/namespaces')
-const setGraph = require('../setGraph')
+import { strictEqual } from 'assert'
+import { array } from 'get-stream'
+import { object } from 'into-stream'
+import { isDuplex } from 'isstream'
+import { describe, it } from 'mocha'
+import rdf from 'rdf-ext'
+import { ex } from './support/namespaces.js'
+import setGraph from '../setGraph.js'
 
 describe('setGraph', () => {
   it('should be a factory', () => {
@@ -13,40 +13,40 @@ describe('setGraph', () => {
   })
 
   it('should return a duplex stream', () => {
-    const stream = setGraph(ns.ex.graph)
+    const stream = setGraph(ex.graph)
 
     strictEqual(isDuplex(stream), true)
   })
 
   it('should set the graph of all quads', async () => {
     const quads = [
-      rdf.quad(ns.ex.subject1, ns.ex.predicate1, ns.ex.object1, ns.ex.graph1),
-      rdf.quad(ns.ex.subject2, ns.ex.predicate2, ns.ex.object2, ns.ex.graph2)
+      rdf.quad(ex.subject1, ex.predicate1, ex.object1, ex.graph1),
+      rdf.quad(ex.subject2, ex.predicate2, ex.object2, ex.graph2)
     ]
 
-    const map = setGraph(ns.ex.graph)
+    const map = setGraph(ex.graph)
 
-    intoStream.object(quads).pipe(map)
+    object(quads).pipe(map)
 
-    const result = await getStream.array(map)
+    const result = await array(map)
 
-    strictEqual(ns.ex.graph.equals(result[0].graph), true)
-    strictEqual(ns.ex.graph.equals(result[1].graph), true)
+    strictEqual(ex.graph.equals(result[0].graph), true)
+    strictEqual(ex.graph.equals(result[1].graph), true)
   })
 
   it('should accept string values', async () => {
     const quads = [
-      rdf.quad(ns.ex.subject1, ns.ex.predicate1, ns.ex.object1, ns.ex.graph1),
-      rdf.quad(ns.ex.subject2, ns.ex.predicate2, ns.ex.object2, ns.ex.graph2)
+      rdf.quad(ex.subject1, ex.predicate1, ex.object1, ex.graph1),
+      rdf.quad(ex.subject2, ex.predicate2, ex.object2, ex.graph2)
     ]
 
-    const map = setGraph(ns.ex.graph.value)
+    const map = setGraph(ex.graph.value)
 
-    intoStream.object(quads).pipe(map)
+    object(quads).pipe(map)
 
-    const result = await getStream.array(map)
+    const result = await array(map)
 
-    strictEqual(ns.ex.graph.equals(result[0].graph), true)
-    strictEqual(ns.ex.graph.equals(result[1].graph), true)
+    strictEqual(ex.graph.equals(result[0].graph), true)
+    strictEqual(ex.graph.equals(result[1].graph), true)
   })
 })

zazuko/barnard59

packages/rdf/test/setGraph.test.js
@@ -1,11 +1,11 @@
-const { strictEqual } = require('assert')
-const getStream = require('get-stream')
-const { isDuplex } = require('isstream')
-const { describe, it } = require('mocha')
-const rdf = require('rdf-ext')
-const { Readable } = require('readable-stream')
-const ns = require('./support/namespaces')
-const setGraph = require('../setGraph')
+import { strictEqual } from 'assert'
+import getStream from 'get-stream'
+import { isDuplex } from 'isstream'
+import { describe, it } from 'mocha'
+import rdf from 'rdf-ext'
+import { Readable } from 'readable-stream'
+import * as ns from './support/namespaces.js'
+import setGraph from '../setGraph.js'
 
 describe('setGraph', () => {
   it('should be a factory', () => {