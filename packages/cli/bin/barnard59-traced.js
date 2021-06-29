854eb1fc7bacefbf3020ae9352d30586bd51cc56
Quentin Gliech <quentingliech@gmail.com>
2021-06-29 10:57:47 +0200

    feat: opentelemetry tracing

---
+++ packages/cli/bin/barnard59-traced.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
+import { CollectorTraceExporter, CollectorMetricExporter } from '@opentelemetry/exporter-collector'
+import { Resource } from '@opentelemetry/resources'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { ResourceAttributes } from '@opentelemetry/semantic-conventions'
+
+const traceExporter = new CollectorTraceExporter()
+const metricExporter = new CollectorMetricExporter()
+
+const sdk = new NodeSDK({
+  autoDetectResources: true,
+  traceExporter,
+  metricExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+  resource: new Resource({
+    [ResourceAttributes.SERVICE_NAME]: 'barnard59'
+  })
+})
+
+sdk.start()
+  .then(() => import('../cli.js'))
+  .then(({ main }) => main())
+  .finally(() => sdk.shutdown())
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
+  .then(() => sdk.shutdown())
--- packages/cli/bin/barnard59.js
+++ packages/cli/bin/barnard59.js
@@ -1,73 +1,9 @@
 #!/usr/bin/env node
 
-import { createWriteStream } from 'fs'
-import { dirname, resolve } from 'path'
-import program from 'commander'
-import rdf from 'rdf-ext'
-import fromFile from 'rdf-utils-fs/fromFile.js'
-import findPipeline from '../findPipeline.js'
-import bufferDebug from '../lib/bufferDebug.js'
-import runner from '../runner.js'
+import { main } from '../cli.js'
 
-async function fileToDataset (filename) {
-  return rdf.dataset().import(fromFile(filename))
-}
-
-function createOutputStream (output) {
-  if (output === '-') {
-    return process.stdout
-  }
-
-  return createWriteStream(output)
-}
-
-function setVariable (str, all) {
-  let [key, value] = str.split('=', 2)
-
-  if (typeof value === 'undefined') {
-    value = process.env[key]
-  }
-
-  return all.set(key, value)
-}
-
-program
-  .command('run <filename>')
-  .option('--output [filename]', 'output file', '-')
-  .option('--pipeline [iri]', 'IRI of the pipeline description')
-  .option('--variable <name=value>', 'variable key value pairs', setVariable, new Map())
-  .option('--variable-all', 'Import all environment variables')
-  .option('-v, --verbose', 'enable diagnostic console output', (v, total) => ++total, 0)
-  .option('--enable-buffer-monitor', 'enable histogram of buffer usage')
-  .action(async (filename, { output, pipeline: iri, variable: variables, variableAll, verbose, enableBufferMonitor } = {}) => {
-    try {
-      const level = ['error', 'info', 'debug'][verbose] || 'error'
-
-      const dataset = await fileToDataset(filename)
-      const ptr = findPipeline(dataset, iri)
-
-      if (variableAll) {
-        for (const [key, value] of Object.entries(process.env)) {
-          variables.set(key, value)
-        }
-      }
-
-      const { finished, pipeline } = await runner(ptr, {
-        basePath: resolve(dirname(filename)),
-        level,
-        outputStream: createOutputStream(output),
-        variables
-      })
-
-      if (enableBufferMonitor) {
-        bufferDebug(pipeline)
-      }
-
-      await finished
-    } catch (err) {
-      console.error(err)
-      process.exit(1)
-    }
+main()
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
   })
-
-program.parse(process.argv)
