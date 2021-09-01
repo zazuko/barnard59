995e67e1d053be31d52914dce783d8faf57c7ff4
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-09-01 11:09:41 +0200

    style: fix standard

--- bin/barnard59.js
+++ bin/barnard59.js
@@ -73,7 +73,7 @@ const onError = async err => {
   if (otelMetricsExporter === 'otlp') {
     const exporter = new CollectorMetricExporter()
     sdk.configureMeterProvider({
-      exporter,
+      exporter
     })
   }
 
