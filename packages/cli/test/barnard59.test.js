e5807788728255f00573a1ee456d7b64cd00db81
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-04-17 11:15:16 +0200

    revert: local changes

--- packages/cli/test/barnard59.test.js
+++ packages/cli/test/barnard59.test.js
@@ -112,9 +112,9 @@ describe('barnard59', () => {
 
     it('should run the parse-csvw.ttl example without error', () => {
       const pipelineFile = (new URL('../examples/parse-csvw.ttl', import.meta.url)).pathname
-      const command = `${barnard59} run -vv --pipeline=http://example.org/pipeline/parseCsvw ${pipelineFile}`
+      const command = `${barnard59} run --pipeline=http://example.org/pipeline/parseCsvw ${pipelineFile}`
 
-      const result = shell.exec(command)
+      const result = shell.exec(command, { silent: true })
 
       strictEqual(result.code, 0)
     })
