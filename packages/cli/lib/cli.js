1ad86597224137a8519d48f04f7ebce0839c00fd
Giacomo Citi <giacomo.citi@zazuko.com>
2023-09-14 16:04:02 +0200

    move desugaring to cli

--- packages/cli/lib/cli.js
+++ packages/cli/lib/cli.js
@@ -1,4 +1,6 @@
 import { program } from 'commander'
+import rdf from '@zazuko/env'
+import ns from './namespaces.js'
 import runAction from './cli/runAction.js'
 import * as monitoringOptions from './cli/monitoringOptions.js'
 import * as commonOptions from './cli/commonOptions.js'
@@ -17,13 +19,59 @@ program
   .addOption(monitoringOptions.metricsInterval)
   .addOption(monitoringOptions.tracesExporter)
 
+const discoverOperations = async () => {
+  const ops = rdf.termMap()
+  for await (const { manifest } of discoverManifests()) {
+    manifest
+      .has(rdf.ns.rdf.type, ns.p.Operation)
+      .forEach(x => { // distinguish EcmaScript and EcmaScriptModule?
+        const link = x.out(ns.code.implementedBy).out(ns.code.link).term
+
+        ops.set(x.term, link)
+      })
+  }
+
+  return ops
+}
+
+export const desugarWith = context => ptr => {
+  const { knownOperations } = context
+  let n = 0
+  for (const step of ptr.has(ns.rdf.type, ns.p.Pipeline)
+    .out(ns.p.steps)
+    .out(ns.p.stepList)
+    .list()) {
+    if (step.out(ns.code.implementedBy).terms.length === 0) {
+      // when there is no implementation, we expect a known operation
+      const [quad] = step.dataset.match(step.term)
+      const knownStep = knownOperations.get(quad.predicate)
+      if (knownStep) {
+        const args = step.out(quad.predicate)
+        step.deleteOut(quad.predicate)
+        step.addOut(ns.code.arguments, args)
+        step.addOut(ns.rdf.type, ns.p.Step)
+        const moduleNode = ptr.blankNode(`impl_${n++}`)
+        moduleNode.addOut(ns.rdf.type, ns.code.EcmaScriptModule)
+        moduleNode.addOut(ns.code.link, knownStep)
+        step.addOut(ns.code.implementedBy, moduleNode)
+      }
+    }
+  }
+  return ptr
+}
+
+const desugar = async ptr => {
+  const knownOperations = await discoverOperations()
+  return desugarWith({ knownOperations })(ptr)
+}
+
 const runCommand = program
   .command('run <filename>')
   .option('--output [filename]', 'output file', '-')
   .option('--pipeline [iri]', 'IRI of the pipeline description')
   .action(async (filename, options) => {
     const { basePath, ptr } = await parse(filename, options.pipeline)
-    return runAction(ptr, basePath, options)
+    return runAction(await desugar(ptr), basePath, options)
   })
 
 export default async function () {
--- packages/cli/test/simplify.test.js
+++ packages/cli/test/simplify.test.js
@@ -2,55 +2,23 @@ import { expect } from 'chai'
 import rdf from '@zazuko/env'
 import toCanonical from 'rdf-dataset-ext/toCanonical.js'
 import { pipelineDefinitionLoader } from 'barnard59-test-support/loadPipelineDefinition.js'
-import ns from '../lib/namespaces.js'
+import { desugarWith } from '../lib/cli.js'
 
 const loadPipelineDefinition = pipelineDefinitionLoader(import.meta.url)
 
-ns.code = rdf.namespace('https://code.described.at/')
-ns.base = rdf.namespace('http://example.org/pipeline/')
-
-const knownImplementations = rdf.termMap([
+// test double for a fake context
+const knownOperations = rdf.termMap([
   [rdf.namedNode('http://barnard59.zazuko.com/operations/concat'), rdf.namedNode('node:barnard59-base#concat')],
 ])
 
-// test double for a fake context
-const tryGetKnownStep = x => knownImplementations.get(x)
-
-// to be moved
-const desugar = context => ptr => {
-  const { tryGetKnownStep } = context
-  let n = 0
-  for (const step of ptr.has(ns.rdf.type, ns.p.Pipeline)
-    .out(ns.p.steps)
-    .out(ns.p.stepList)
-    .list()) {
-    if (step.out(ns.code.implementedBy).terms.length === 0) {
-      // when there is no implementation, we expect a known
-      const [quad] = step.dataset.match(step.term)
-      const knownStep = tryGetKnownStep(quad.predicate)
-      if (knownStep) {
-        const args = step.out(quad.predicate)
-        step.deleteOut(quad.predicate)
-        step.addOut(ns.code.arguments, args)
-        step.addOut(ns.rdf.type, ns.p.Step)
-        const moduleNode = ptr.blankNode(`impl_${n++}`)
-        moduleNode.addOut(ns.rdf.type, ns.code.EcmaScriptModule)
-        moduleNode.addOut(ns.code.link, knownStep)
-        step.addOut(ns.code.implementedBy, moduleNode)
-      }
-    }
-  }
-  return ptr.dataset
-}
-
 describe('simplified syntax', () => {
-  it.only('should desugar simplified syntax', async () => {
+  it('should desugar simplified syntax', async () => {
     const expected = await loadPipelineDefinition('simplified-step-args-expected')
     const pipeline = await loadPipelineDefinition('simplified-step-args')
-    const sut = desugar({ tryGetKnownStep })
+    const sut = desugarWith({ knownOperations })
 
     const result = sut(pipeline)
 
-    expect(toCanonical(result)).to.eq(toCanonical(expected.dataset))
+    expect(toCanonical(result.dataset)).to.eq(toCanonical(expected.dataset))
   })
 })
