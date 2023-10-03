ad93d83c3ced44de6c655af8a867e8bcb06eb1d5
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-10-03 16:35:05 +0200

    refactor: HTTPS operation identifiers

---
+++ .changeset/smooth-crews-travel.md
@@ -0,0 +1,15 @@
+---
+"barnard59-graph-store": major
+"barnard59-formats": major
+"barnard59-sparql": major
+"barnard59-base": major
+"barnard59-core": major
+"barnard59-csvw": major
+"barnard59-http": major
+"barnard59-ftp": major
+"barnard59-rdf": major
+"barnard59-s3": minor
+"barnard59-test-e2e": patch
+---
+
+Change the operation URLs to be HTTPS (re zazuko/barnard59-website#4)
--- packages/cli/test/simplify.test.js
+++ packages/cli/test/simplify.test.js
@@ -11,7 +11,7 @@ const loadPipelineDefinition = pipelineDefinitionLoader(import.meta.url)
 
 // test double for a fake context
 const knownOperations = rdf.termMap([
-  [rdf.namedNode('http://barnard59.zazuko.com/operations/concat'), {
+  [rdf.namedNode('https://barnard59.zazuko.com/operations/concat'), {
     type: rdf.namedNode('https://code.described.at/EcmaScriptModule'),
     link: rdf.namedNode('node:barnard59-base#concat'),
   }],
