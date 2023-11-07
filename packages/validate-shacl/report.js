ae3e2625c51155bf43dd59ac7a29f548f50a0710
Giacomo Citi <giacomo.citi@zazuko.com>
2023-11-07 10:59:38 +0100

    fix shacl report

--- packages/validate-shacl/report.js
+++ packages/validate-shacl/report.js
@@ -1,31 +1,33 @@
+import { Duplex } from 'stream'
 import { isStream, isReadableStream } from 'is-stream'
 import SHACLValidator from 'rdf-validate-shacl'
-import { Transform } from 'readable-stream'
 
-class ValidateChunk extends Transform {
-  constructor(context, shape) {
-    super({
-      writableObjectMode: true,
-      readableObjectMode: true,
-    })
-    this.context = context
-    this.validator = new SHACLValidator(shape, { maxErrors: 0, factory: context.env })
-  }
-
-  _transform(data, encoding, callback) {
-    const report = this.validator.validate(data)
+async function * validate(iterable, validator, maxViolations) {
+  let violations = 0
+  for await (const chunk of iterable) {
+    const report = validator.validate(chunk)
     if (!report.conforms) {
-      callback(null, report.dataset)
+      yield report.dataset
+      violations = violations + report.results.filter(r => r.severity.value === 'http://www.w3.org/ns/shacl#Violation').length
+      if (maxViolations && violations > maxViolations) {
+        throw new Error(`limit of ${maxViolations} violations exceeded. ${violations} violations found`)
+      }
     }
+  }
+  if (violations) {
+    throw new Error(`${violations} violations found`)
   }
 }
 
 export async function shacl(arg) {
   let shape
+  let options
+  let maxErrors
   if (isStream(arg)) {
     shape = arg
   } else if (arg) {
-    ({ shape } = arg)
+    ({ shape, ...options } = arg)
+    maxErrors = options.maxErrors < 1 ? undefined : Number(options.maxErrors)
   }
 
   if (!shape) {
@@ -35,5 +37,7 @@ export async function shacl(arg) {
     throw new Error(`${shape} is not a readable stream`)
   }
 
-  return new ValidateChunk(this, await this.env.dataset().import(shape))
+  const ds = await this.env.dataset().import(shape)
+  const validator = new SHACLValidator(ds, { maxErrors: 0, factory: this.env })
+  return Duplex.from(iterable => validate(iterable, validator, maxErrors))
 }
