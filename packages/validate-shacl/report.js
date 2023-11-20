6c60476727061b5ba7895079d46c63bcfe659210
tpluscode <tpluscode@users.noreply.github.com>
2023-11-20 13:37:34 +0100

    refactor: no stdout

--- packages/validate-shacl/report.js
+++ packages/validate-shacl/report.js
@@ -2,35 +2,32 @@ import { Duplex } from 'stream'
 import { isStream, isReadableStream } from 'is-stream'
 import SHACLValidator from 'rdf-validate-shacl'
 
-async function * validate(iterable, validator, maxViolations) {
-  let violations = 0
+async function * validate(validator, maxViolations, iterable) {
   for await (const chunk of iterable) {
-    if (maxViolations && violations >= maxViolations) {
-      continue // skip validation but continue to avoid finalization issues
+    const totalViolations = this.variables.get('violations')
+    if (maxViolations && totalViolations > maxViolations) {
+      this.logger.warn('Reached max violations. Aborting')
+      break
     }
+
     const report = validator.validate(chunk)
     if (!report.conforms) {
-      violations = violations + report.results.filter(r => r.severity.value === 'http://www.w3.org/ns/shacl#Violation').length
+      const violations = report.results.filter(r => this.env.ns.sh.Violation.equals(r.severity)).length
+      this.variables.set('violations', totalViolations + violations)
       yield report.dataset
     }
-  }
-  if (violations && maxViolations) {
-    throw new Error(`At least ${violations} violations found`)
-  }
-  if (violations) {
-    throw new Error(`${violations} violations found`)
   }
 }
 
 export async function shacl(arg) {
   let shape
   let options
-  let maxErrors
+  let maxViolations
   if (isStream(arg)) {
     shape = arg
   } else if (arg) {
     ({ shape, ...options } = arg)
-    maxErrors = options.maxErrors < 1 ? undefined : Number(options.maxErrors)
+    maxViolations = options.maxErrors < 1 ? undefined : Number(options.maxErrors)
   }
 
   if (!shape) {
@@ -42,5 +39,9 @@ export async function shacl(arg) {
 
   const ds = await this.env.dataset().import(shape)
   const validator = new SHACLValidator(ds, { maxErrors: 0, factory: this.env })
-  return Duplex.from(iterable => validate(iterable, validator, maxErrors))
+
+  this.variables.set('violations', 0)
+  this.variables.set('maxViolations', maxViolations)
+
+  return Duplex.from(validate.bind(this, validator, maxViolations))
 }
