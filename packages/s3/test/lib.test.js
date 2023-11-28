a518617f8415fa4a1bbd32dcf5767bfe96aa71c6
Ludovic Muller <ludovicmuller1@gmail.com>
2023-11-28 10:12:35 +0100

    tests: some tests for the client lib

---
+++ packages/s3/test/lib.test.js
@@ -0,0 +1,46 @@
+// @ts-check
+import { strictEqual, deepEqual } from 'node:assert'
+import { S3Client } from '@aws-sdk/client-s3'
+import { generateConfig } from '../lib/client.js'
+
+describe('lib', async () => {
+  describe('client', async () => {
+    it('should be able to create a new client', async () => {
+      const client = new S3Client()
+      strictEqual(client instanceof S3Client, true)
+    })
+
+    it('should be able to generate a config with default values', async () => {
+      const generatedConfig = generateConfig({})
+      deepEqual(generatedConfig, {
+        region: 'us-east-1',
+      })
+    })
+
+    it('should be able to override default region', async () => {
+      const generatedConfig = generateConfig({
+        region: 'eu-central-1',
+      })
+      deepEqual(generatedConfig, {
+        region: 'eu-central-1',
+      })
+    })
+
+    it('should be able to override default region', async () => {
+      const generatedConfig = generateConfig({
+        endpoint: 'http://localhost',
+        accessKeyId: 'accessKeyId',
+        secretAccessKey: 'secretAccessKey',
+        region: 'eu-central-1',
+      })
+      deepEqual(generatedConfig, {
+        endpoint: 'http://localhost',
+        credentials: {
+          accessKeyId: 'accessKeyId',
+          secretAccessKey: 'secretAccessKey',
+        },
+        region: 'eu-central-1',
+      })
+    })
+  })
+})
