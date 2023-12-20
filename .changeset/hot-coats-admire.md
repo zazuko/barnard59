89fa4fbcc7345361f07bc3a27eef3ab93cbfee52
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-12-20 17:14:30 +0100

    fix: find barnard59.js when running global installation

---
+++ .changeset/hot-coats-admire.md
@@ -0,0 +1,5 @@
+---
+"barnard59": patch
+---
+
+Fix: running from global installation did not work
--- packages/cli/bin/barnard59.sh
+++ packages/cli/bin/barnard59.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-SCRIPT_PATH=$(dirname "$0")
+# find local barnard59
+barnard59=$(node -e "console.log(require.resolve('barnard59/bin/barnard59.js'))" 2> /dev/null)
+
+if [ -z "$barnard59" ]
+then
+  # find global barnard59
+  NODE_PATH=$(npm config get prefix)
+  barnard59=$(node -e "console.log(require('path').join('$NODE_PATH', '/lib/node_modules/barnard59/bin/barnard59.js'))")
+fi
 
 # if ts-node exists in path
 if command -v ts-node &> /dev/null
 then
   # use ts-node
-  node --loader ts-node/esm/transpile-only --no-warnings "$SCRIPT_PATH"/../barnard59/bin/barnard59.js "$@"
+  node --loader ts-node/esm/transpile-only --no-warnings "$barnard59" "$@"
 else
   # use plain node
-  node "$SCRIPT_PATH"/../barnard59/bin/barnard59.js "$@"
+  node "$barnard59" "$@"
 fi
