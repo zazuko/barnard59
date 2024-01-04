c5abf0c9f8ecccd9c60ea8f0e6a681e34faaab7f
Giacomo Citi <giacomo.citi@zazuko.com>
2024-01-04 09:34:44 +0100

    Update packages/base/test/Readable.test.js

--- packages/base/test/Readable.test.js
+++ packages/base/test/Readable.test.js
@@ -1,13 +1,17 @@
 import assert from 'node:assert'
 import { isReadableStream } from 'is-stream'
+import getStream from 'get-stream'
+import { expect } from 'chai'
 import * as Readable from '../Readable.js'
 
 describe('Readable', () => {
-  it('should return a readable stream', () => {
+  it('should return a readable stream', async () => {
     // given
-    const stream = Readable.from([0, 1, 2, 3, 4])
+    const stream = Readable.from(0, 1, 2, 3, 4)
 
     // then
     assert(isReadableStream(stream))
+    const array = await getStream.array(stream)
+    expect(array).to.deep.equal([0, 1, 2, 3, 4])
   })
 })
