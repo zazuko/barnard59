3972ec180d5625f05134cadabfb046e14ff48604
Giacomo Citi <giacomo.citi@zazuko.com>
2024-02-08 13:31:16 +0100

    fix: type annotations

--- packages/shacl/lib/TermCounter.js
+++ packages/shacl/lib/TermCounter.js
@@ -1,8 +1,14 @@
 export default class TermCounter {
+  /**
+   * @param {import('@rdfjs/term-map/Factory.js').TermMapFactory} env
+   */
   constructor(env) {
     this.termMap = env.termMap()
   }
 
+  /**
+   * @param {import('@rdfjs/types').Term} term
+   */
   add(term) {
     this.termMap.set(term, (this.termMap.get(term) ?? 0) + 1)
   }
