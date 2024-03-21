f6d593c15ba730122f885be3dafd33a6ebeb7f9b
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-03-21 12:37:36 +0100

    test: large uploads

---
+++ .changeset/quick-lamps-pull.md
@@ -0,0 +1,5 @@
+---
+"barnard59-base": patch
+---
+
+`limit` step would not break the pipeline when the stream produces a very lare or infinite amount of data
--- packages/graph-store/test/pipeline.test.js
+++ packages/graph-store/test/pipeline.test.js
@@ -6,10 +6,12 @@ import { pipelineDefinitionLoader } from 'barnard59-test-support/loadPipelineDef
 import env from 'barnard59-env/index.ts'
 import { createPipeline } from 'barnard59-core/index.ts'
 import getStream from 'get-stream'
+import { SELECT } from '@tpluscode/sparql-builder'
+import { fromRdf } from 'rdf-literal'
 
 const support = (new URL('./support', import.meta.url)).pathname
 
-const loadPipelineDefinition = pipelineDefinitionLoader(import.meta.url, '../pipeline')
+const loadPipelineDefinition = pipelineDefinitionLoader(import.meta.url, '../')
 
 const user = 'admin'
 const password = 'admin'
@@ -27,20 +29,21 @@ describe('graph-store pipeline', function () {
   })
 
   const graph = 'http://example.org/'
+  const client = new ParsingClient({
+    factory: env,
+    endpointUrl: endpoint + '/query',
+    updateUrl: endpoint + '/update',
+    user,
+    password,
+  })
 
   const selectAll = () => {
-    const client = new ParsingClient({
-      factory: env,
-      endpointUrl: endpoint + '/query',
-      user,
-      password,
-    })
     return client.query.construct('CONSTRUCT { ?s ?p ?o } WHERE { ?s ?p ?o }')
   }
 
   it('should run graph-store put pipeline without error', async () => {
     const data = `${support}/data.ttl`
-    const { ptr, basePath } = await loadPipelineDefinition('put', {
+    const { ptr, basePath } = await loadPipelineDefinition('pipeline/put', {
       term: env.namedNode('https://barnard59.zazuko.com/pipeline/graph-store/put'),
     })
     const pipeline = createPipeline(ptr, {
@@ -60,5 +63,46 @@ describe('graph-store pipeline', function () {
     const storedData = await selectAll()
     const expectedData = await env.dataset().import(env.fromFile(data))
     strictEqual(storedData.toCanonical(), expectedData.toCanonical())
+  })
+
+  context('given large dataset', () => {
+    const graph = 'http://example.org/large-graph'
+
+    beforeEach(async function () {
+      this.timeout(10000)
+      await client.query.update(`DROP GRAPH <${graph}>`)
+    });
+
+    [1000, 10000, 100000].forEach((size) => {
+      context(`with ${size} triples`, () => {
+        it('should complete graph store operation in a timely manner', async function () {
+          this.timeout(10000)
+
+          // given`
+          const { ptr, basePath } = await loadPipelineDefinition('test/support/pipelines/large-dataset')
+          const pipeline = createPipeline(ptr, {
+            env,
+            basePath,
+            variables: new Map([
+              ['endpoint', endpoint],
+              ['user', user],
+              ['password', password],
+              ['graph', graph],
+              ['triple-count', size],
+            ]),
+          })
+
+          // when
+          await getStream(pipeline.stream)
+
+          // then
+          const [{ count }] = await SELECT`(count(*) as ?count)`
+            .FROM(env.namedNode(graph))
+            .WHERE`?s ?p ?o`
+            .execute(client)
+          strictEqual(fromRdf(count), size)
+        })
+      })
+    })
   })
 })
