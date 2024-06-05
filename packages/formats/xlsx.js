fd6d0366eb1ea40b36728120631ee796f32a1f55
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-06-05 14:39:24 +0200

    feat: trim csv headers

--- packages/formats/xlsx.js
+++ packages/formats/xlsx.js
@@ -1,5 +1,5 @@
 import sinkToDuplex from '@rdfjs/sink-to-duplex'
-import CsvwXlsxParser from 'rdf-parser-csvw-xlsx'
+import CsvwXlsxParser from '@zazuko/rdf-parser-csvw-xlsx'
 import tracer from './lib/tracer.js'
 import { toDataset } from './lib/stream.js'
 
