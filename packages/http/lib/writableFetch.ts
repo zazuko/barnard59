3c51a243c36ba075bec6cee0806c474507670911
tpluscode <tpluscode@users.noreply.github.com>
2024-08-06 21:43:32 +0200

    revert: do not mix stream and readable-stream

--- packages/http/lib/writableFetch.ts
+++ packages/http/lib/writableFetch.ts
@@ -1,20 +1,22 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Duplex } from 'node:stream'
-import { PassThrough, Readable } from 'node:stream'
+import { PassThrough } from 'readable-stream'
 import duplexify from 'duplexify'
+import type { RequestInit } from 'node-fetch'
+import fetch from 'node-fetch'
 import tracer from './tracer.js'
 
 export type PostInit = RequestInit & { url: string }
 
 export default async function writableFetch({ method = 'POST', url, ...options }: PostInit): Promise<Duplex> {
   const inputStream: any = new PassThrough()
-  const outputStream = new PassThrough()
+  const outputStream: any = new PassThrough()
 
   tracer.startActiveSpan('writableFetch', span => setTimeout(async () => {
     try {
       const response = await fetch(url, { method, body: inputStream, ...options })
 
-      Readable.fromWeb(response.body as any).pipe(outputStream)
+      response.body!.pipe(outputStream)
     } catch (err) {
       outputStream.emit('error', err)
     } finally {
